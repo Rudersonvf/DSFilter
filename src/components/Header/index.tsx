c536714b7657b4c1de06f69f1e818b19464f8f1c
Ruderson Rolemar <suporte2@rolemar.com>
2023-07-24 13:42:05 -0300

    Project created, clean, Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export default function Header() {
+    return(
+        <header>
+            <nav>
+            <h1 className="ft-wt-bd">DSFilter</h1>
+            <p className="ft-wt-bd">6 produto(s)</p>
+            </nav>
+        </header>
+    );
+}
