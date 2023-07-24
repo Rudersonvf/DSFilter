d5723107a2d13558f6544f563a9b8e411e7303b7
Ruderson Rolemar <suporte2@rolemar.com>
2023-07-24 15:19:05 -0300

    Listing component

---
+++ src/components/Listing/Index.tsx
@@ -0,0 +1,32 @@
+import "./style.css";
+
+export default function Listing() {
+    return (
+        <div className="df-list-container">
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+            <div className="df-list-card">
+                <h2>Pc Gamer Top</h2>
+                <p>R$ 1200,00</p>
+            </div>
+        </div>
+    );
+}
--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,10 +1,12 @@
 import Filter from "../Filter";
+import Listing from "../Listing/Index";
 import "./styles.css";
 
 export default function ListingBody(){
     return(
         <main>
-            <Filter/>
+            <Filter />
+            <Listing />
         </main>
     );
 }
